101d43f00b7c181e73fa1fe48e07b8200e6fe709
martingim <martingimse@gmail.com>
2021-02-18 11:37:23 +0100

    comments, changes and bug fixes

--- surface_height.m
+++ surface_height.m
@@ -1,25 +1,40 @@
 close all
-
+f = 2.25; %The frequency of the run
+[omega,T,K,LAMBDA,CP,CG] = wparam(f, 0.33); %calculate the wavenumber and phase velocity
+sensor_distance = 0.4; %the dstance between the sensors in meters(should maybe change this to a vector)
 %start and stop frame for measuring the elevation of the surface
 surface_frame_start = 589;
 surface_frame_end = 2074;
 
 sensor_samplerate = 100; %Hz
-min_separation = 1/frequency*sensor_samplerate*0.8;
+min_separation = 1/f*sensor_samplerate*0.8; %minimum separation when finding the crests and troughs
 %read sensor data from file
-sensordata = table2array(readtable(folder + 'run2_3.csv'));
+sensordata = table2array(readtable(folder + 'run4_3.csv'));
 
 %remove zeros
 sensordata = sensordata(:,5:8);
-sensordata(sensordata<100) = NaN;
+sensordata(sensordata<100|sensordata>900) = NaN; %replace the outliers with NaN
 
-
+%convert the data drom the sensors to measured surface elevation in meters
 measured_surface = [sensor1(sensordata(:,1)), sensor2(sensordata(:,2)) sensor3(sensordata(:,3)) sensor4(sensordata(:,4))];
+
+%subtract the mean from the surface to get zero mean
 surface_mean = mean(measured_surface, 'omitnan');
-%surface_mean = mean(surface_mean);
 measured_surface = measured_surface -surface_mean;
 
-sensor_offset = round(frequency*sensor_samplerate); %sensor offset in frames
+%plot the sensor data from all four sensors with the same waves on top of
+%each other
+sensor_offset = round(sensor_samplerate*sensor_distance/CP)-2; %sensor offset in samples to plot the same waves on top of each other
+figure;
+hold on 
+plot(measured_surface(:,1))
+plot(measured_surface(1*sensor_offset:end,2))
+plot(measured_surface(2*sensor_offset:end,3))
+plot(measured_surface(3*sensor_offset:end,4))
+legend('sensor1', 'sensor2', 'sensor3', 'sensor4')
+
+%Find the crests on troughs from the first sensor and plot with the
+%measured data
 time = (1:size(measured_surface, 1))/100;
 figure;
 hold on
@@ -29,33 +44,56 @@ LMax = islocalmax(measured_surface(:,1), 'MinSeparation', min_separation);
 LMin = islocalmin(measured_surface(:,1), 'MinSeparation', min_separation);
 plot(time(LMin), surf(LMin), 'x')
 plot(time(LMax), surf(LMax), 'x')
-figure;
-hold on 
-plot(measured_surface(sensor_offset:end,2))
-plot(measured_surface(sensor_offset*2:end,3))
-plot(measured_surface(sensor_offset*3:end,4))
 
 
 %%
-surf = measured_surface(surface_frame_start:surface_frame_end,:);
-mean_amplitude = [];
+%find the crests and troughs of the measured surface to find the mean
+%amplitude of the waves. %%maybe change this later to only look at one wave
+surf = measured_surface(surface_frame_start:surface_frame_end,:); %change this to use the sensor_offset
+mean_amplitude = zeros(size(surf, 2),1);
 for i=1:size(surf, 2)
     LMax = islocalmax(surf(:,i), 'MinSeparation', min_separation);
     LMin = islocalmin(surf(:,i), 'MinSeparation', min_separation);
-    mean_amplitude = [mean_amplitude (mean(surf(LMax,i))-mean(surf(LMin, i)))/2];
+    mean_amplitude(i) = (mean(surf(LMax,i))-mean(surf(LMin, i)))/2;
 end
 
+a = mean(mean_amplitude);
 
-%% FFT of the signal
-X = fillmissing(sensordata(:,1),'linear');
-Y = fft(X);
+t = 0:0.001:2;
+eta = a*cos(-omega*t); %the linear surface
+eta_non_linear = a*cos(-omega*t) +  0.5*a^2*K*cos(2*(-omega*t)) + 3/8*K^2*a^3*cos(3*(-omega*t)); %the non-linear surface
 
-L = size(sensordata, 1);
+%find the first crest to start plotting from
+crests = islocalmax(surf(:,1), 'MinSeparation', 10);
+[m, start_idx] = max(crests); %the index to start plotting the measured surface from
+
+%plot the surfaces
+figure
+hold on
+plot(t, eta)
+plot(t, eta_non_linear)
+t_measured = 0:0.01:2;
+plot(t_measured, surf(start_idx:start_idx+size(t_measured, 2)-1,1)')
+legend('linear', 'non-linear', 'measured')
+
+
+%% FFT of the measured surface
+X = fillmissing(surf(:,2),'linear');
+Y = fft(X); 
+
+L = size(surf, 1);
 P2 = abs(Y/L);
 P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 Fs = 100;
-f = Fs*(0:(L/2))/L;
+frequencies = Fs*(0:(L/2))/L;
 
 figure;
-plot(f, P1)
+hold on
+plot(frequencies, P1)
+plot(f, 0, 'x')
+plot(2*f, 0, 'x')
+plot(3*f, 0, 'x')
+legend('fft of the measured surface', 'f', '2f', '3f')
+xlabel('frequency[Hz]')
+xlim([0 f*3.2])
