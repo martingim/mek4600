ced911af507350babce236c7896d736745489acd
Martin <martingimse@gmail.com>
2021-03-14 16:21:45 +0100

    script for plotting all the velocites of all the runs

---
+++ ex2/plot_all_v.m
@@ -0,0 +1,11 @@
+function [] = plot_all_v()
+%UNTITLED6 Summary of this function goes here
+%   Detailed explanation goes here
+close all
+n_runs = 15;
+for run_number = 1:n_runs
+    plot_run(run_number)
+end
+
+end
+
